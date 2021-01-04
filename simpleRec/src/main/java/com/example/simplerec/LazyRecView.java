a753f3b139e4734136f95fc2f0f46a6c212d3ded
narekcyber777 <53946598+narekcyber777@users.noreply.github.com>
2021-01-05 00:50:48 +0400

    Added new Methods

--- simpleRec/src/main/java/com/example/simplerec/LazyRecView.java
+++ simpleRec/src/main/java/com/example/simplerec/LazyRecView.java
@@ -14,6 +14,27 @@ import java.util.List;
 
 public class LazyRecView extends RecyclerView implements LazyRecyclerView {
     private List<RecyclerView.ItemDecoration> itemDecorationList;
+    private RecyclerView recyclerView;
+
+
+    public LazyRecView(RecyclerView recyclerView, Context context) {
+        super(context);
+        acceptRec(recyclerView);
+
+    }
+
+    public LazyRecView with(RecyclerView recyclerView) {
+        this.recyclerView = recyclerView;
+        return this;
+    }
+
+    private void acceptRec(RecyclerView recyclerView) {
+        if (recyclerView == null) {
+            this.recyclerView = this;
+        } else {
+            this.recyclerView = recyclerView;
+        }
+    }
 
 
     public LazyRecView(@NonNull Context context) {
@@ -33,6 +54,9 @@ public class LazyRecView extends RecyclerView implements LazyRecyclerView {
     }
 
     private final void afterCreate() {
+        if (recyclerView == null) {
+            acceptRec(null);
+        }
         itemDecorationList = new ArrayList<>();
 
     }
@@ -45,7 +69,7 @@ public class LazyRecView extends RecyclerView implements LazyRecyclerView {
 
     @Override
     public RecyclerView getRecyclerView() {
-        return this;
+        return recyclerView;
     }
 
     public void doStuff() {
@@ -54,13 +78,13 @@ public class LazyRecView extends RecyclerView implements LazyRecyclerView {
 
     @Override
     public LazyRecyclerView lazySetLayout(RecyclerView.LayoutManager layoutManager) {
-        setLayoutManager(layoutManager);
+        recyclerView.setLayoutManager(layoutManager);
         return this;
     }
 
     @Override
     public LazyRecyclerView lazyFixedSize(boolean answer) {
-        setHasFixedSize(answer);
+        recyclerView.setHasFixedSize(answer);
 
         return this;
     }
@@ -68,13 +92,13 @@ public class LazyRecView extends RecyclerView implements LazyRecyclerView {
     @Override
     public LazyRecyclerView lazySetEmptyLinearLayout() {
 
-        setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         return this;
     }
 
     @Override
     public LazyRecyclerView lazySetAdapter(RecyclerView.Adapter adapter) {
-        setAdapter(adapter);//
+        recyclerView.setAdapter(adapter);//
         return this;
     }
 
@@ -82,12 +106,13 @@ public class LazyRecView extends RecyclerView implements LazyRecyclerView {
     public LazyRecyclerView lazyAddItemDecoration(RecyclerView.ItemDecoration itemDecoration) {
         super.addItemDecoration(itemDecoration);
         itemDecorationList.add(itemDecoration);
+        recyclerView.addItemDecoration(itemDecoration);
         return this;
     }
 
     @Override
     public LazyRecyclerView setLayoutParams(RecyclerView.LayoutParams layoutParams) {
-        setLayoutParams(layoutParams);
+        recyclerView.setLayoutParams(layoutParams);
         return this;
     }
 
@@ -101,37 +126,37 @@ public class LazyRecView extends RecyclerView implements LazyRecyclerView {
 
     @Override
     public LazyRecyclerView scrollToPos(int pos) {
-        scrollToPosition(pos);
+        recyclerView.scrollToPosition(pos);
         return this;
     }
 
     @Override
     public LazyRecyclerView smoothScrollToPos(int pos) {
-        super.smoothScrollToPosition(pos);
+        recyclerView.smoothScrollToPosition(pos);
         return null;
     }
 
     @Override
     public LazyRecyclerView scrollToXY(int x, int y) {
-        scrollTo(x, y);
+        recyclerView.scrollTo(x, y);
         return null;
     }
 
     @Override
     public LazyRecyclerView scrollByXY(int x, int y) {
-        scrollBy(x, y);
+        recyclerView.scrollBy(x, y);
         return null;
     }
 
     @Override
     public LazyRecyclerView smoothScrollByXY(int x, int y) {
-        smoothScrollBy(x, y);
+        recyclerView.smoothScrollBy(x, y);
         return null;
     }
 
     @Override
     public LazyRecyclerView lazyRemoveAdapter() {
-        setAdapter(null);
+        recyclerView.setAdapter(null);
 
         return this;
     }
